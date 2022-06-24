bdf77080dbbd3c9ea4cd285aafe1de0c85f24610
MissNat81 <91033142+MissNat81@users.noreply.github.com>
2022-06-24 15:27:40 -0700

    Update front-page.php
    Commented out template-parts/content & comments_template()

--- front-page.php
+++ front-page.php
@@ -23,12 +23,12 @@ get_header();
 			?>
 			<h1><?php the_title(); ?></h1>
 			<?php
-			get_template_part( 'template-parts/content', 'page' );
+			// get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			// // If comments are open or we have at least one comment, load up the comment template.
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
 
 		endwhile; // End of the loop.
 		?>
