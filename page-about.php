0e81c79a6728c6c4884a2a2100be6b0b4999b50e
Jessica <jessicalaporte888@gmail.com>
2022-06-26 12:15:33 -0700

    Output content About page

--- page-about.php
+++ page-about.php
@@ -16,18 +16,38 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-		<h1>Page to be worked on (Jessica)</h1>
-
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			if ( function_exists ( 'get_field' ) ) {
+				if ( get_field( 'about_us_header' )){
+					echo '<h1>';
+					the_field( 'about_us_header');
+					echo '</h1>';
+				}
+				if ( get_field( 'about_us_info' )){
+					echo '<p>';
+					the_field( 'about_us_info');
+					echo '</p>';
+				}
+				if ( get_field( 'about_us_image_1' ) ) {
+					echo wp_get_attachment_image( get_field( 'about_us_image_1' ), 'medium', '', array('class' => 'custom-class' ));
+				}
+				if ( get_field( 'about_us_image_2' ) ) {
+					echo wp_get_attachment_image( get_field( 'about_us_image_2' ), 'medium', '', array('class' => 'custom-class' ));
+				}
+				if ( get_field( 'about_us_image_3' ) ) {
+					echo wp_get_attachment_image( get_field( 'about_us_image_3' ), 'medium', '', array('class' => 'custom-class' ));
+				}
+			}
+
+			// get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
 
 		endwhile; // End of the loop.
 		?>
