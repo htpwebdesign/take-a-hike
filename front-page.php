b55ea18c78e992f95adf31adccd148075918f671
MissNat81 <91033142+MissNat81@users.noreply.github.com>
2022-06-26 07:51:17 -0700

    added ACF titles to front-page.php

--- front-page.php
+++ front-page.php
@@ -31,7 +31,7 @@ get_header();
 					<?php
 					if (function_exists ( 'get_field' )) :
 					?>
-						<h2>Hiking</h2>
+						<h2><?php the_field('activity_title_1'); ?></h2>
 					<?php
 						$image = get_field('hiking_image');
 						$size = 'medium'; // (thumbnail, medium, large, full or custom size)
@@ -46,7 +46,7 @@ get_header();
 					<?php
 					if (function_exists ( 'get_field' )) :
 					?>
-						<h2>Camping</h2>
+						<h2><?php the_field('activity_title_2'); ?></h2>
 						<?php
 						$image = get_field('camping_image');
 						$size = 'medium'; // (thumbnail, medium, large, full or custom size)
@@ -61,7 +61,7 @@ get_header();
 					<?php
 					if (function_exists ( 'get_field' )) :
 					?>
-						<h2>Climbing</h2>
+						<h2><?php the_field('activity_title_3'); ?></h2>
 					<?php
 						$image = get_field('climbing_image');
 						$size = 'medium'; // (thumbnail, medium, large, full or custom size)
@@ -76,7 +76,7 @@ get_header();
 					<?php
 					if (function_exists ( 'get_field' )) :
 					?>
-						<h2>Watersports</h2>
+						<h2><?php the_field('activity_title_4'); ?></h2>
 					<?php
 						$image = get_field('watersports_image');
 						$size = 'medium'; // (thumbnail, medium, large, full or custom size)
@@ -92,7 +92,7 @@ get_header();
 				<?php
 					if (function_exists ( 'get_field' )) :
 					?>
-						<h2>Featured Products</h2>
+						<h2><?php the_field('featured_products_title'); ?></h2>
 					<?php
 						$image = get_field('featured_products_image');
 						$size = 'large'; // (thumbnail, medium, large, full or custom size)
@@ -112,7 +112,7 @@ get_header();
 				<?php
 					if (function_exists ( 'get_field' )) :
 					?>
-						<h2>Sale</h2>
+						<h2><?php the_field('sale_title'); ?></h2>
 					<?php
 						$image = get_field('sale_image');
 						$size = 'large'; // (thumbnail, medium, large, full or custom size)
@@ -126,7 +126,7 @@ get_header();
 				<?php
 					if (function_exists ( 'get_field' )) :
 					?>
-						<h2>Bundles</h2>
+						<h2><?php the_field('bundles_title'); ?></h2>
 					<?php
 						$image = get_field('bundles_image');
 						$size = 'large'; // (thumbnail, medium, large, full or custom size)
