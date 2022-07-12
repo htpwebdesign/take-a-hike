49e2b2cfeddbfe9ca79dae8aad9dcebf8d1dbb50
MissNat81 <91033142+MissNat81@users.noreply.github.com>
2022-07-12 14:41:33 -0700

    added Block Editor Styles to functions.php

--- functions.php
+++ functions.php
@@ -267,3 +267,30 @@ function my_login_form() { ?>
 	</style> 
 <?php } 
 add_action( 'login_enqueue_scripts', 'my_login_form' );
+
+
+/**
+ * Block Editor Styles
+ */
+
+add_editor_style();
+add_theme_support( 'editor-style' );
+
+
+// function add_editor_style( $stylesheet = 'editor-style.css' ) {
+//     global $editor_styles;
+ 
+//     add_theme_support( 'editor-style' );
+
+// 	$editor_styles = (array) $editor_styles;
+//     $stylesheet    = (array) $stylesheet;
+ 
+//     if ( is_rtl() ) {
+//         $rtl_stylesheet = str_replace( '.css', '-rtl.css', $stylesheet[0] );
+//         $stylesheet[]   = $rtl_stylesheet;
+//     }
+ 
+//     $editor_styles = array_merge( $editor_styles, $stylesheet );
+// }
+
+
