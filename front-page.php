d02c191133420960ad7e9670c19ded8696bd25cf
MissNat81 <91033142+MissNat81@users.noreply.github.com>
2022-07-13 12:33:22 -0700

    corrected indentation in front-page.php

--- front-page.php
+++ front-page.php
@@ -15,33 +15,29 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		
+	<main id="primary" class="site-main">		
 		
 		<?php
 		while ( have_posts() ) :
 			the_post();
 			?>
 			<section class="banner-section">
-					<?php
-					if (function_exists ( 'get_field' ) ) :
-					$image = get_field('banner_image');
-					$size = 'full'; // (thumbnail, medium, large, full or custom size)
-						if( $image ) :
+				<?php
+				if ( function_exists ( 'get_field' ) ) :
+				$image = get_field('banner_image');
+				$size = 'full'; // (thumbnail, medium, large, full or custom size)
+					if( $image ) :
 						echo wp_get_attachment_image( $image, $size );					
-						endif; ?>	
-						<h1><?php the_title(); ?></h1>			
-					<?php
-					endif;
-					?>
+					endif; 
+					?>	
+					<h1><?php the_title(); ?></h1>			
 			</section>
 		
 			<section class="main-section">
 				<section class="hero-section">				
-					<section class="hero-grid">
+					<div class="hero-grid">
 						<?php
-						if (have_rows ( 'hero_section' ) ) :
+						if ( have_rows ( 'hero_section' ) ) :
 							while( have_rows( 'hero_section' ) ): 
 								the_row(); 
 								$link = get_sub_field( 'link' );
@@ -75,7 +71,7 @@ get_header();
 							endwhile;
 						endif;
 						?>
-					</section>
+					</div>
 				</section>
 
 				<section class="mid-section">
@@ -121,12 +117,10 @@ get_header();
 					
 				<section class="end-section">
 					<?php
-					if ( function_exists( 'get_field' ) ) :
-						if ( get_field( 'workshop_subheading' ) ) :
-						?>							
-							<h2 class="workshop-header"><?php the_field( 'workshop_subheading' ); ?></h2>
-							<?php
-						endif;
+					if ( get_field( 'workshop_subheading' ) ) :
+					?>							
+						<h2 class="workshop-header"><?php the_field( 'workshop_subheading' ); ?></h2>
+						<?php
 					endif;
 
 					$args = array (
@@ -136,42 +130,42 @@ get_header();
 						'order' => 'ASC'
 					);			
 
-						$query = new WP_Query($args);
+					$query = new WP_Query($args);
 			
-						if ( $query->have_posts() ) :
-							while ( $query->have_posts() ) :
-							$query->the_post();
-							?>
-    							<a class="activity-link" href="<?php the_permalink(); ?>">
-									<div class="main-container"> 
-										<?php the_post_thumbnail( 'large' ); ?>
-										<h2 class="title"><?php the_title(); ?></h2>
-									</div>
-   							 	</a>	
+					if ( $query->have_posts() ) :
+						while ( $query->have_posts() ) :
+						$query->the_post();
+						?>
+    						<a class="activity-link" href="<?php the_permalink(); ?>">
+								<div class="main-container"> 
+									<?php the_post_thumbnail( 'large' ); ?>
+									<h2 class="title"><?php the_title(); ?></h2>
+								</div>
+   							</a>	
 							
-							<?php  
-							endwhile;
-							wp_reset_postdata();
-						endif;
+						<?php  
+						endwhile;
+						wp_reset_postdata();
+					endif;
 					?>
 				</section>		
 					
 				<section class="social-section">				
-					<?php
-						if (function_exists ( 'get_field' )) :
-						?>
-							<h2 class="social-header"><?php the_field( 'instagram_subheading' ); ?></h2>
-							<?php echo do_shortcode('[instagram-feed feed=1]'); ?>
-						<?php
-						endif;
+				<?php
+					if( get_field( 'instagram_subheading' ) ) :
 					?>
-				
+						<h2 class="social-header"><?php the_field( 'instagram_subheading' ); ?></h2>
+						<?php echo do_shortcode('[instagram-feed feed=1]'); ?>
+					<?php
+					endif;
+				?>				
 				</section>
+
 			</section>
 			<?php
+				endif;
 		endwhile; // End of the loop.
 		?>
-
 	</main><!-- #main -->
 
 <?php
