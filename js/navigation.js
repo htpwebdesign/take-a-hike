d9d5a5aae5579dc254ad2c91b94ef916e516b469
Jessica <jessicalaporte888@gmail.com>
2022-07-14 14:42:33 -0700

    Fixed mobile navigation (shop) and added aria-label to svg

--- js/navigation.js
+++ js/navigation.js
@@ -101,7 +101,6 @@
 
 		if ( event.type === 'touchstart' ) {
 			const menuItem = this.parentNode;
-			event.preventDefault();
 			for ( const link of menuItem.parentNode.children ) {
 				if ( menuItem !== link ) {
 					link.classList.remove( 'focus' );
